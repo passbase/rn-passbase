f7ca255a581675386a5f96876c8d976acaaf69e6
Kamran Younis <join.kamran92@gmail.com>
2020-01-03 19:19:59 +0500

    update example to use latest version of passbase

--- example/ios/empty.swift
+++ example/ios/empty.swift
@@ -2,8 +2,8 @@
 //  empty.swift
 //  example
 //
-//  Created by Macbook Pro on 13/12/2019.
-//  Copyright © 2019 Facebook. All rights reserved.
+//  Created by Kamran Younis on 03/01/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
 //
 
 import Foundation
